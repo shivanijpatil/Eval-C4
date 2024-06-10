46a91e6887fb1298a9305558fcd3d2f01be786dc
shivanijpatil <shivanipatil786@gmail.com>
2024-06-10 18:26:19 +0530

    eval 4

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const uploadData = require('./scripts/uploadData');
 const app = express();
-const port = 4000;
+const port = 3000;
 
 
 app.use(express.static(path.join(__dirname, 'dashboard')));
--- src/db/database.js
+++ src/db/database.js
@@ -1,9 +1,9 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://shivanipatil786:root@<cluster>/<dbname>?retryWrites=true&w=majority';
 const dbName = 'monitoring';
 
 const connectDB = async () => {
-    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(url);
     await client.connect();
     const db = client.db(dbName);
     return { db, client };
